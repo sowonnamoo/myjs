39aadde58620e606b76886b554aa396e55ee604d
sowonnamoo <sowonnamoo@gmail.com>
2026-07-18 20:55:01 +0900

    Update delivery.js

--- products/delivery.js
+++ products/delivery.js
@@ -7,19 +7,16 @@ function calculateDeliveryDate(daysToAdd) {
         "2026-08-15", "2026-09-24", "2026-09-25", "2026-09-26",
         "2026-10-03", "2026-10-09", "2026-12-25"
     ];
-
     let date = new Date();
     let added = 0;
-
     while (added < daysToAdd) {
         date.setDate(date.getDate() + 1);
         const isWeekend = (date.getDay() === 0 || date.getDay() === 6);
         const dateStr = date.toISOString().split('T')[0];
         const isHoliday = holidays.includes(dateStr);
-
         if (!isWeekend && !isHoliday) {
             added++;
         }
     }
-    return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate() + 1}일`;
+    return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
 }
